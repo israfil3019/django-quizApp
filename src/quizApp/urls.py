7553eacba1572acc1d11a89995bd4fa3076b670d
israfil <israfil3019@gmail.com>
2021-08-27 11:45:23 +0300

    use rest-frame for categories

--- src/quizApp/urls.py
+++ src/quizApp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import home
+from .views import category, home
 from django.conf import settings
 from django.conf.urls.static import static
 
 
-
 urlpatterns = [
     path('home', home, name="home"),
+    path('category/', category),
+
 ]
 if settings.DEBUG:
      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- src/quizApp/views.py
+++ src/quizApp/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import HttpResponse
+from .models import Category
+from .serializers import CategorySerializer
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
+
+
 
 def home(request):
     return HttpResponse('<h1>Home Page</h1>')
 
+
+@api_view(['GET', 'POST'])
+def category(request):
+    if request.method == 'GET':
+        categories = Category.objects.all()
+        serializer = CategorySerializer(categories, many=True)
+      
+        return Response(serializer.data)
+  
