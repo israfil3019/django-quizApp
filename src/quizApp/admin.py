ddc4ac4ddeb306f61bf4eb7bcaded358062ca4ee
israfil <israfil3019@gmail.com>
2021-08-27 11:41:31 +0300

    add models to admin panel

--- src/quizApp/admin.py
+++ src/quizApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Choice, Question, Quiz
+
+
+admin.site.register(Category)
+admin.site.register(Choice)
+admin.site.register(Question)
+admin.site.register(Quiz)
